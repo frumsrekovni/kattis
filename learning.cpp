efb0920f2c0ae395874de5c8f917bcd4b744acf5
frumsrekovni <joshua.backstrom@gmail.com>
2022-05-06 17:33:03 +0200

    Got interrupted this thought has to continue later

---
+++ learning.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int8_t a=129; //
+uint8_t b=100; //
+char c = 25;
+using namespace std;
+
+int main(){
+    uint8_t b=100;
+    std::cout<<"a="<<(int)a;
+    std::cout<<", b="<<(int)b;
+    return 0;
+}
