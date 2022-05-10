bb5c1e3fac26ef1ce133adc4bc56d1df232ef829
frumsrekovni <joshua.backstrom@gmail.com>
2022-05-10 13:33:35 +0200

    Just some more tinkering

--- 2naire/main.cpp
+++ 2naire/main.cpp
@@ -3,19 +3,25 @@ using namespace std;
 
 int main(){
     int amount_of_questions = 0;
-    float correct_prob = 0.0;
-    float tot_prize = 1.0; // Player starts with a dollar
+    double correct_prob = 0.0;
+    double current_tot_prize = 1.0; // Player starts with a dollar
+    double potential_tot_prize = 1.0;
+
     do
     {
         cin >> amount_of_questions >> correct_prob;
-        for (int i = 1; i < amount_of_questions; i++)
+        // This calcs the prize money for the question that he is on
+        for (int i = 0; i < amount_of_questions; i++)
         {
-            tot_prize *= 2;
+            potential_tot_prize *= 2;
             //caught myself halfway through that im literally just typing out pow() manually
             // Will keep it as is for posterity's sake
         }
-        cout << tot_prize; // This is the total prize pool after inserted amount of questions
-
+        current_tot_prize = potential_tot_prize/2;
+        
+        cout << "Current prize: "<< current_tot_prize<< endl; // This is the total prize pool after inserted amount of questions
+        cout << "Potential prize: "<<potential_tot_prize << endl;
+        
     } while ((amount_of_questions != 0) && (correct_prob != 0) );
     
     return 0;
