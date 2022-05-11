816eb4257b047148224586647fe362e8a2a00af5
frumsrekovni <joshua.backstrom@gmail.com>
2022-05-11 23:26:49 +0200

    Started helpme. Just a simple template for now

---
+++ helpme/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*
+    White player UPPERCASE. Black player lowercase.
+    The chess board is 8x8. So a nested for loop sounds reasonable.
+    */
+    for (int i = 0; i < 8; i++)
+    {
+        
+        for (int y = 0; y < 8; y++)
+        {
+            /*Parse an entire column*/
+        }
+        
+    }
+    
+    cout << "White: \n" << "Black: ";
+
+    return 0;
+}
