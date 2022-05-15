16703463bef0eb73e5c3f836d6f5461979a6f08d
frumsrekovni <joshua.backstrom@gmail.com>
2022-05-15 23:49:10 +0200

    Building the outputs and printing them out works. Just have to sort them

--- helpme/main.cpp
+++ helpme/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int main(){
@@ -11,6 +12,9 @@ int main(){
     ifstream myfile ("1.in");
     string in;
     char firstchar;
+    char columns[] = {'a','b','c','d','e','f','g','h'};
+    vector<string> wPieces;
+    vector<string> bPieces;
     while(myfile >>firstchar){
         if(firstchar == '+'){ // "+" indicates that the entire line is useless
             getline(myfile, in);
@@ -18,28 +22,37 @@ int main(){
         }
         else{
             // If not "+" but in this case actually always the "|" char we have coming useful info
-
-            for (int i = 0; i < 8; i++)
+            for (int u = 0; u < 8; u++)
             {
-                // i value here represent the 1 in "Ke1". aka what row the piece is in
-                /* There are 8 squares */
-                for (int y = 0; y < 4; y++)
+                // u value is the 1 in "Ke1". aka what row the piece is in
+                for (int i = 0; i < 8; i++)
                 {
-                    /* There are 4 chars per square */
-                    char read_char;
-                    myfile >> read_char;
-                    if(isupper(read_char)){
-                        cout << "This is a White piece";
+                    // i value here represent the e in "Ke1". aka what column the piece is in
+                    /* There are 8 squares */
+                    for (int y = 0; y < 4; y++)
+                    {
+                        /* There are 4 chars per square */
+                        char read_char;
+                        int curr_row;
+                        myfile >> read_char;
+                        if(isupper(read_char)){
+                            curr_row = abs(u - 8);
+                            //cout << read_char << columns[i] << curr_row << endl;
+                            string chesspiece = "";
+                            
+                            chesspiece = chesspiece + read_char + columns[i] + char(curr_row);
+                            
+                        }
+                        else if(islower(read_char)){
+                            curr_row = abs(u - 8);
+                            read_char = toupper(read_char);
+                            cout << read_char << columns[i] << curr_row << endl; 
+                        }
                     }
-                    else if(islower(read_char)){
-                        cout << "This is a Black piece";
-                    }
-                    cout << read_char;
+                    // A row has 2 lines of chars.
                 }
-                // A row has 2 lines of chars.
             }
         }
-        cout <<endl;
     }
     
     
