2e1772f783512b7ac936d5a06782d98845e24183
frumsrekovni <joshua.backstrom@gmail.com>
2022-05-31 23:45:15 +0200

    Just some ideas on calories

--- calories/main.cpp
+++ calories/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 // 3g 10g 10% 0g 0g
 // FAT PROTEIN SUGAR STARCH ALCOHOL
@@ -17,27 +18,54 @@ using namespace std;
     Answer: 32% of Calories is from fat
 */
 int main(){
-    string input = "";
+    string inputline = "";
     int totCalories(0);
-    while(getline(cin, input)){
-        // Every new line is a food/test
-        char a;
-        int individualCalories(0);
-        int step(0);
-        for (int i = 0; i < input.length(); i++)
-        {
-            // Really should separate the whole input string into its individual 5 elements and after that do the conversions and calculations
-            a = input.at(i);
-            if(a == 'g')
+    int fatPercentage = 0;
+    while(getline(cin, inputline)){
+        if(inputline != "-"){
+            // Every new line is a food/test
+            int individualValue(0);
+            istringstream ss(inputline);
+            string nutrition = "";
+            char unit;
+            int percentageLeft = 0;
+            for (int i = 0; i < 5; i++)
             {
-
+                ss >> nutrition;
+                unit = nutrition.back();
+                nutrition.pop_back();
+                individualValue = stoi(nutrition);
+                if(unit == 'g'){
+                    if(i == 0){ // Fat
+                        individualValue *= 9;
+                    }
+                    else if(i > 0 && i < 4){ // Protein, sugar and starch
+                        individualValue *= 4;
+                    }
+                    else if(i == 4){ // Alcohol
+                        individualValue *= 7;
+                    }
+                    totCalories += individualValue;
+                }
+                else if(unit == '%'){
+                    if(i == 0){
+                        fatPercentage = individualValue;
+                    }
+                    else{
+                        percentageLeft = individualValue;
+                    }
+                }
+                else if(unit == 'C'){
+                    totCalories += individualValue;
+                }
             }
-            else
-            {
-
-            }
-
+            // Here one food is done
+            
         }
+        else{
+            cout << fatPercentage << "%\n";
+        }
+        
     }
     return 0;
 }
