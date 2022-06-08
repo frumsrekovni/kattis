e854f3799ee5ad177d0e713382e40b815e0ffcc9
frumsrekovni <joshua.backstrom@gmail.com>
2022-06-08 19:38:33 +0200

    Inching forward

--- helpme/main.cpp
+++ helpme/main.cpp
@@ -6,13 +6,6 @@
 #include <map>
 using namespace std;
 
-
-struct allPieceSort{
-    explicit allPieceSort(bool black): isBlack(black) {}
-    bool isBlack;
-};
-
-
 bool chessPieceSort(string piece1, string piece2){
     // Print order: K > Q > R > B > N > P
     // If same type and white piece: smaller row > larger row
@@ -38,7 +31,20 @@ bool chessPieceSort(string piece1, string piece2){
     // cout << "2: " <<  char_hierarchy.at(piece2.at(0))<<endl;
     return char_hierarchy.at(piece1.at(0)) > char_hierarchy.at(piece2.at(0));
     // cout << piece1.at(0);
-    return true;
+}
+bool chessPieceSort2(string piece1, string piece2){
+    map<char, int> char_hierarchy { {'K', 10}, {'Q', 9}, {'R', 5}, {'B', 3}, {'N', 2}};
+    if(piece1.size() > piece2.size()){
+        return true;
+    }
+    else if(piece1.size() < piece2.size()){
+        return false; 
+    }
+
+    if(piece1.size() == 2){
+        return piece1.at(1) > piece2.at(1); // This is the only thing that needs to be flipped for black sort
+    }
+    return char_hierarchy.at(piece1.at(0)) > char_hierarchy.at(piece2.at(0));
 }
 int main(){
     /*
@@ -53,13 +59,14 @@ int main(){
     vector<string> bPieces;
     int currentline = 1;
     int currentRow = 8;
-    while(myfile >>firstchar){
+    while(currentRow > 0){
+        cin >>firstchar;
         if(currentline > 2){
             currentline = 1;
             currentRow--;
         }
         if(firstchar == '+'){ // "+" indicates that the entire line is useless
-            getline(myfile, in);
+            getline(cin, in);
             currentline++;
         }
         else if(firstchar == '|'){
@@ -75,7 +82,7 @@ int main(){
                     int curr_row;
                     int curr_col;
                     string chesspiece = "";
-                    myfile >> read_char;
+                    cin >> read_char;
                     if(isupper(read_char)){
                         if(read_char == 'P'){
                             // In here it's a pawn. Meaning dont add the readchar to the chesspiece output
@@ -105,16 +112,22 @@ int main(){
     }
     
     sort(wPieces.begin(),wPieces.end(), chessPieceSort);
-     cout << "White: ";
-     for (int i = 0; i < wPieces.size(); i++)
-     {
-         cout << wPieces.at(i) << ",";
-     }
-     sort(bPieces.begin(),bPieces.end(), chessPieceSort);
-     cout << "\nBlack: ";
-     for (int i = 0; i < bPieces.size(); i++)
-     {
-         cout << bPieces.at(i) << ",";
-     }
+    cout << "White: ";
+    for (size_t i = 0; i < wPieces.size(); i++)
+    {
+        cout << wPieces.at(i);
+        if(i+1 != wPieces.size()){ // last element should not print a comma
+        cout << ",";
+        }
+    }
+    sort(bPieces.begin(),bPieces.end(), chessPieceSort2);
+    cout << "\nBlack: ";
+    for (size_t i = 0; i < bPieces.size(); i++)
+    {
+        cout << bPieces.at(i);
+        if(i+1 != bPieces.size()){ // last element should not print a comma
+        cout << ",";
+        }
+    }
     return 0;
 }
